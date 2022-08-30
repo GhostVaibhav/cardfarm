11398a2526bbc910d76f9ec04b8067c7561c10da
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2022-08-30 21:26:47 +0530

    Initial setup

---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Card() {
+    return (
+        <div className="flex">
+            <div className="flex flex-col">
+                <div>
+                    Vaibhav Sharma
+                </div>
+                <div>
+                    Student
+                </div>
+                <div>
+                    Hi! I am Vaibhav Sharma. I am a student of B.Tech. I am a web developer and a competitive programmer.
+                </div>
+            </div>
+            <div>
+                <div>
+                    Links
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
